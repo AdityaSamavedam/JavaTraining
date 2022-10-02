a5edc26667b60f2c3092034762a136a6d15fe87d
AdityaSamavedam <114468365+AdityaSamavedam@users.noreply.github.com>
2022-10-02 10:10:21 -0700

    Add files via upload

---
+++ arithmeticsexercise by Aditya Samavedam.java
@@ -0,0 +1,7 @@
+public class arithmeticsexercise {
+    public static void main(String[] args) {
+        int num = ((6*1234124)%11345)-890809+90800;
+        double num1 = num/100;
+        System.out.println(num1);
+    }
+}
